0667b7756548235f5e8940a8da73f9cc8f86e2e3
Nicolas H <hubnersantos@aol.com>
2025-07-01 18:26:09 -0300

    feat Add new logger

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
 const itemsRouter = require('./routes/items');
 const statsRouter = require('./routes/stats');
 const cors = require('cors');
 const { getCookie, notFound } = require('./middleware/errorHandler');
+const logger = require('./middleware/logger');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,7 +11,7 @@ const port = process.env.PORT || 3001;
 app.use(cors({ origin: 'http://localhost:3000' }));
 // Basic middleware
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(logger);
 
 // Routes
 app.use('/api/items', itemsRouter);
